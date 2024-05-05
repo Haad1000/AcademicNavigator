e40497eaf1e56bde706fb0f8b02e4c9b2b5ed40f
Haad1000 <115592978+Haad1000@users.noreply.github.com>
2024-05-05 00:46:58 -0500

    Initialized the frontend

--- academic-navigator/tailwind.config.js
+++ academic-navigator/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // This tells Tailwind where to look for class names.
+  ],
   theme: {
     extend: {},
   },
