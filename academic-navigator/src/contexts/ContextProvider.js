12ab496c7d94a254c27ef44eedcff7606b99bf6e
Haad1000 <115592978+Haad1000@users.noreply.github.com>
2024-05-08 14:53:28 -0500

    finish sidebar and navbar

--- academic-navigator/src/contexts/ContextProvider.js
+++ academic-navigator/src/contexts/ContextProvider.js
@@ -16,8 +16,13 @@ export const ContextProvider = ({ children }) => {
 
   const [isClicked, setIsClicked] = useState(initialState);
 
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true});
+  }
+  const [screenSize, setScreenSize] = useState(undefined)
+
   return (
-    <StateContext.Provider value={{ activeMenu, setActiveMenu, isClicked, setIsClicked }}>
+    <StateContext.Provider value={{ activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize }}>
       {children}
     </StateContext.Provider>
   );
