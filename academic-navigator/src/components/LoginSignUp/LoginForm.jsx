85aa874a56523f855e89b7b40de24ee3069e81ae
Haad1000 <115592978+Haad1000@users.noreply.github.com>
2024-05-09 08:50:14 -0500

    idk

--- academic-navigator/src/components/LoginSignUp/LoginForm.jsx
+++ academic-navigator/src/components/LoginSignUp/LoginForm.jsx
@@ -7,39 +7,19 @@ import { useStateContext } from "../../contexts/ContextProvider";
 const LoginForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
   const navigate = useNavigate();
-  const { setUserId } = useStateContext();
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    try {
-      const response = await fetch("http://127.0.0.1:4000/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          usernameOrEmail: username,
-          password: password,
-        }),
-      });
-
-      const data = await response.json();
-
-      if (response.ok && data.user_id) {
-        // Successful login
-        setUserId(data.user_id);
-        navigate("/dashboard"); // Redirect to the dashboard
-      } else if (data.error) {
-        // Error response from the server
-        setError(data.error);
-      } else {
-        setError("Unexpected error occurred.");
-      }
-    } catch (e) {
-      setError("An error occurred while trying to log in."); // Handle unexpected errors
+    const responce = await fetch('http://127.0.0.1:4000/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({ username, password })
+    });
+    const data = await responce.json();
+    if (responce.ok) {
+      console.log(responce)
     }
   };
 
@@ -54,12 +34,6 @@ const LoginForm = () => {
       <div className="wrapper">
         <form onSubmit={handleLogin}>
           <h1>Login</h1>
-
-          {error && (
-            <div className="error-message">
-              {error}
-            </div>
-          )}
 
           <div className="input-box">
             <input
